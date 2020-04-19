ec6f8092c68f39d08bde093e9318977778ab7fb9
cjb230 <james.barton@blueyonder.co.uk>
2020-04-19 06:16:43 +0200

    e27, new routines in primes.py

--- python/e27.py
+++ python/e27.py
@@ -1,3 +1,4 @@
+import datetime
 import math
 
 PRIMES_DICT = {1: 2, 2: 3, 3: 5}
@@ -29,22 +30,6 @@ def prime_by_number(prime_requested):
         return generate_prime(prime_requested)
     else:
         return PRIMES_DICT[prime_requested]
-
-
-def prime_factors(factorisation_requested):
-    global PRIMES_DICT
-    prime_factors = dict()
-    residual = factorisation_requested
-    next_prime_check_index = 1
-    while residual > 1:
-        this_prime_power = 0
-        while residual % PRIMES_DICT[next_prime_check_index] == 0:
-            this_prime_power += 1
-            residual = int(residual / PRIMES_DICT[next_prime_check_index])
-        if this_prime_power > 0:
-            prime_factors[PRIMES_DICT[next_prime_check_index]] = this_prime_power
-        next_prime_check_index += 1
-    return prime_factors
 
 
 def in_primes_dict(check_value):
@@ -97,5 +82,37 @@ def is_prime(check_value):
     return result
 
 
+def main():
+    global PRIMES_DICT
+    prime_index = 4
+    next_prime = generate_prime(prime_index)
+    while next_prime < 1001:
+        prime_index += 1
+        next_prime = generate_prime(prime_index)
+    possible_b = {v for v in PRIMES_DICT.values() if v < 1000}
+    longest_run_length = 0
+    longest_run_a = -1000
+    longest_run_b = -1
+    for this_b in possible_b:
+        for a in range(-1000, 1000):
+            n = 0
+            while True:
+                x = (n * (n + a)) + this_b
+                if not(is_prime(x)):
+                    break
+                else:
+                    if n > longest_run_length:
+                        longest_run_length = n
+                        longest_run_a = a
+                        longest_run_b = this_b
+                    n += 1
+
+    print(longest_run_a * longest_run_b)
+
+
 if __name__ == "__main__":
-    print(prime_by_number(10001))
+    start = datetime.datetime.now()
+    main()
+    end = datetime.datetime.now()
+    duration = end - start
+    print(str(duration.total_seconds()) + " seconds")
